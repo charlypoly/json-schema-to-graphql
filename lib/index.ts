cf0215025d54afcef6d0e31facc37a9a364ab634
Charly POLY <cpoly55@gmail.com>
2021-09-13 16:06:17 +0200

    feat(convert): single ref defintion support for GQL transformation

--- lib/index.ts
+++ lib/index.ts
@@ -3,145 +3,160 @@ import {
   GraphQLFieldConfigMap,
   GraphQLInt,
   GraphQLList,
+  GraphQLNonNull,
   GraphQLObjectType,
   GraphQLOutputType,
   GraphQLString,
   printType,
-} from "graphql";
-import {
-  JSONSchema6,
-  JSONSchema6Definition,
-  JSONSchema6TypeName,
-} from "json-schema";
+} from 'graphql'
+import { JSONSchema6, JSONSchema6TypeName } from 'json-schema'
 
-import * as assert from "assert";
+import * as assert from 'assert'
 
-const schema: JSONSchema6 = {
-  $schema: "http://json-schema.org/draft-06/schema#",
-  $ref: "#/definitions/Product",
-  definitions: {
-    Product: {
-      type: "object",
-      additionalProperties: false,
-      properties: {
-        collection_ids: {
-          type: "array",
-          items: {
-            type: "null",
-          },
-        },
-        collections: {
-          type: "array",
-          items: {
-            type: "null",
-          },
-        },
-        title: {
-          type: "string",
-        },
-        salePrice: {
-          type: "integer",
-        },
-        objectID: {
-          type: "string",
-        },
-        shipping: {
-          type: "string",
-        },
-      },
-      required: [
-        "collection_ids",
-        "collections",
-        "objectID",
-        "salePrice",
-        "title",
-      ],
-      title: "Product",
-    },
-  },
-};
+function visitSchema(
+  schema: JSONSchema6,
+  visitProperty: (path: string, definition: JSONSchema6, required: boolean) => void,
+  requiredFields: string[],
+  namespace?: string
+) {
+  for (const [key, property] of Object.entries(schema.properties || {})) {
+    const value = property as JSONSchema6
+    const pathKey = namespace ? `${namespace}.${key}` : key
+    if (value.type === 'object') {
+      visitSchema(value, visitProperty, value.required || [], pathKey)
+    } else {
+      visitProperty(pathKey, value, requiredFields.includes(key))
+    }
+  }
+}
 
-const JSC_ARRAY = Symbol("JSC_ARRAY");
+const JSC_ARRAY = Symbol('JSC_ARRAY')
+
+type JSONSchema6TypeNameSupportedInGQL = Exclude<JSONSchema6TypeName, 'any' | 'null' | 'object'>
+
+const isInputTypeSupportedinGQL = (
+  type: JSONSchema6TypeName
+): type is JSONSchema6TypeNameSupportedInGQL => !['null', 'any', 'object'].includes(type)
 
 const typesMap: {
-  [k in JSONSchema6TypeName]: GraphQLOutputType | typeof JSC_ARRAY | null;
+  [k in JSONSchema6TypeNameSupportedInGQL]: GraphQLOutputType | typeof JSC_ARRAY | null
 } = {
-  any: null,
   array: JSC_ARRAY,
   boolean: GraphQLBoolean,
   integer: GraphQLInt,
-  null: null,
   number: GraphQLInt,
-  object: null,
   string: GraphQLString,
-};
+}
 
-export function convert(input: JSONSchema6): string {
-  assert.strictEqual(
-    input.type,
-    undefined,
-    "This version only supports $ref JSON Schema document"
-  );
+function propertyToGQLType(
+  property: JSONSchema6,
+  key: string,
+  options: ConvertOptions
+): GraphQLOutputType | undefined {
+  if (Array.isArray(property.type)) {
+    console.warn(
+      `skipping prop "${key}", union types are not supported for now (requires references support)`
+    )
+  } else {
+    const propType = property.type
+    if (!propType) {
+      console.warn(`skipping prop "${key}", type is missing`)
+    } else {
+      const gqlType = isInputTypeSupportedinGQL(propType)
+        ? typesMap[propType]
+        : options.fallbackTypes &&
+          options.fallbackTypes[key] &&
+          typesMap[options.fallbackTypes[key]]
+
+      if (!gqlType) {
+        console.warn(
+          `skipping prop "${key}", ${propType} is not supported in GQL (provide a "fallbackTypes" to avoid skipping)`
+        )
+        return
+      } else if (gqlType === JSC_ARRAY) {
+        const items = property.items
+        const gqlItemType = propertyToGQLType(
+          {
+            type: Array.isArray(items)
+              ? (items
+                  .map((i) => (i as JSONSchema6).type)
+                  .filter((t) => !!t) as JSONSchema6TypeName[])
+              : (items as JSONSchema6).type,
+          },
+          `${key}[]`,
+          options
+        )
+        if (gqlItemType) {
+          return new GraphQLList(
+            options.arraysNullable ? gqlItemType : new GraphQLNonNull(gqlItemType)
+          )
+        }
+      } else {
+        return gqlType
+      }
+    }
+  }
+}
+
+interface FallbackTypesMap {
+  [k: string]: JSONSchema6TypeNameSupportedInGQL
+}
+interface ConvertOptions {
+  fallbackTypes?: FallbackTypesMap
+  arraysNullable?: boolean
+}
+
+export function convert(
+  input: JSONSchema6,
+  options: ConvertOptions = {
+    arraysNullable: false,
+    fallbackTypes: {},
+  }
+): string {
+  assert.strictEqual(input.type, undefined, 'This version only supports $ref JSON Schema document')
   assert.notStrictEqual(
     input.$ref,
     undefined,
-    "This version only supports $ref JSON Schema document"
-  );
+    'This version only supports $ref JSON Schema document'
+  )
 
   assert.strictEqual(
     Object.keys(input.definitions || {}).length,
     1,
-    "This version only supports 1 definition"
-  );
+    'This version only supports 1 definition'
+  )
 
-  const typeName = Object.keys(input.definitions || {})[0];
+  const typeName = Object.keys(input.definitions || {})[0]
 
-  const typeDefinition: JSONSchema6 = input.definitions![
-    typeName
-  ] as JSONSchema6;
+  const typeDefinition: JSONSchema6 = input.definitions![typeName] as JSONSchema6
 
   assert.strictEqual(
     typeDefinition.type,
-    "object",
+    'object',
     `Provided "${typeName}" defintion should be an object`
-  );
+  )
 
-  const fields: GraphQLFieldConfigMap<any, any> = {};
+  const fields: GraphQLFieldConfigMap<any, any> = {}
 
-  for (const [key, property] of Object.entries(
-    typeDefinition.properties || {}
-  )) {
-    if (typeof property !== "boolean" && property.type) {
-      const propType = Array.isArray(property.type)
-        ? property.type[0]
-        : property.type;
-      const gqlType = typesMap[propType];
-      // TODO: handle array, non-nullable and references
-      if (gqlType === JSC_ARRAY) {
-        const itemType = Array.isArray((property.items as JSONSchema6).type)
-          ? ((property.items as JSONSchema6).type![0] as JSONSchema6TypeName)
-          : ((property.items as JSONSchema6).type! as JSONSchema6TypeName);
-
-        const gqlItemType = typesMap[itemType];
-        if (gqlItemType && gqlItemType != JSC_ARRAY) {
+  visitSchema(
+    typeDefinition,
+    (key, property, required) => {
+      if (property.type) {
+        const gqlType = propertyToGQLType(property, key, options)
+        if (gqlType) {
           fields[key] = {
-            type: new GraphQLList(gqlItemType),
-          };
+            type: required ? new GraphQLNonNull(gqlType) : gqlType,
+          }
         }
-      } else if (gqlType) {
-        fields[key] = {
-          type: gqlType,
-        };
       }
-    }
-  }
+    },
+    typeDefinition.required || []
+  )
 
   return printType(
     new GraphQLObjectType({
       name: typeName,
       fields,
     })
-  );
+  )
 }
-
-console.log(convert(schema));
